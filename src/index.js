2f0f99621e43a30819e142a8b870e2ae2d83a930
jane baik <janebaik@berkeley.edu>
2021-02-10 19:09:19 -0800

    gathered all data

--- src/index.js
+++ src/index.js
@@ -4,101 +4,118 @@ import {
 } from "./time";
 import "./styles/index.scss";
 
-window.addEventListener("DOMContentLoaded", () => {
-
-    currentLocation();
-
-    // Current location
-    function currentLocation() {
-        debugger
-        if ("geolocation" in navigator) {
-            // debugger
-            // console.log(navigator.geolocation);
-            navigator.geolocation.getCurrentPosition(function (pos) {
-                const api = "f8d77a8717d41a7529bb83ece54c1905";
-                const crd = pos.coords;
-                const latitude = crd.latitude;
-                const longitude = crd.longitude;
-                fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api}`)
-                    .then(function (resp) { return resp.json() })
-                    .then(function (data) {
-                        currentTimeWeather(data)
-                    })
-            }, function (err) {
-                document.getElementById("geolocation-unaviable").innerHTML = err.message;
-            }, { enableHighAccuracy: false, timeout: 5000 });
-        } else {
-            document.getElementById("geolocation-unaviable").innerHTML = "Geolocation is not avaible on your browser, Please type in your city.";
-        }
-    }
-
-    // Search Bar
-    const input = document.querySelector("input");
-    input.addEventListener("change", handleInput);
-    function handleInput(e) {
-        // location based on user's input
-        const input = e.target.value
-        timeConversion(input);
-        weatherSearch(input);
-    }
-
-    function weatherSearch(cityname) {
-        const api = "f8d77a8717d41a7529bb83ece54c1905";
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${api}`)
-            .then(function (resp) { return resp.json() })
-            .then(function (data) {
-                currentTimeWeather(data)
-            })
-    }
-
-    function currentTimeWeather(data) {
+function currentWeather(){
+    if ("geolocation" in navigator) {
         // debugger
-        // current time
-        const unixTime = data.dt + data.timezone;
-        const currentTime = timeConversion(unixTime);
-        document.getElementById("time").innerHTML = currentTime;
-        // current weather
-        const temp = data.main.feels_like;
-        const sky = data.weather[0].description;
-        const location = data.name;
-        const celcius = temp - 273.15;
-        const fahrenheit = 1.8*(temp - 273) + 32
-        document.getElementById("current-temp").innerHTML = `${Math.round(celcius)}°C || ${Math.round(fahrenheit)}°F`;
-        document.getElementById("current-sky").innerHTML = `${sky}`;
-        document.getElementById("location").innerHTML = `${location}`;
+        // console.log(navigator.geolocation);
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            const api = "f8d77a8717d41a7529bb83ece54c1905";
+            const crd = pos.coords;
+            const latitude = crd.latitude;
+            const longitude = crd.longitude;
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api}`)
+                .then(function (resp) { return resp.json() })
+                .then(function (data) {
+                    currentTimeWeather(data)
+                })
+        }, function (err) {
+            document.getElementById("geolocation-unaviable").innerHTML = err.message;
+        }, { enableHighAccuracy: false, timeout: 5000 });
+    } else {
+        document.getElementById("geolocation-unaviable").innerHTML = "Geolocation is not avaible on your browser, Please type in your city.";
     }
+}
+currentWeather();
+function currentTimeWeather(data) {
+    // debugger
+    // current time
+    const unixTime = data.dt + data.timezone;
+    const currentTime = timeConversion(unixTime);
+    document.getElementById("time").innerHTML = currentTime;
+    // current weather
+    const temp = data.main.feels_like;
+    const sky = data.weather[0].description;
+    const location = data.name;
+    const celcius = temp - 273.15;
+    const fahrenheit = 1.8 * (temp - 273) + 32;
+    document.getElementById("current-temp").innerHTML = `${Math.round(celcius)}°C || ${Math.round(fahrenheit)}°F`;
+    document.getElementById("current-sky").innerHTML = `${sky}`;
+    document.getElementById("location").innerHTML = `${location}`;
+}
 
-    // time phases
-    document.getElementById("change-morning").onclick = () =>{
-        const time = ""
-        document.getElementById("time").innerHTML = time
-    }
+const input = document.querySelector("input");
+input.addEventListener("change", handleInput);
+function handleInput(e) {
+    // location based on user's input
+    const input = e.target.value
+    weatherSearch(input);
+}
 
-    document.getElementById("change-afternoon").onclick = function changeMorning() {
-        const time = ""
-        document.getElementById("time").innerHTML = time
-    }
+function weatherSearch(cityname) {
+    const api = "f8d77a8717d41a7529bb83ece54c1905";
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${api}`)
+        .then(function (resp) { return resp.json() })
+        .then(function (data) {
+            currentTimeWeather(data)
+        })
+}
 
-    document.getElementById("change-night").onclick = function changeMorning() {
-        const time = ""
-        document.getElementById("time").innerHTML = time
-    }
+function futureWeather(cityname){
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=${api}`)
+        .then(function (resp) { return resp.json() })
+        .then(function (data) { forecastWeather(data.city.name, data.list) })
+}
 
-    document.getElementById("change-current").onclick = function changeMorning() {
-        // TODO: Implement this in order to gain the current time again
-        debugger
-        document.getElementById("phaseOfDay").innerHTML = currentLocation();
-    }
+function forecastWeather(name, data) {
+    document.getElementById("location").innerHTML = name;
 
-    // NOTE: Both these functions are for ultimate scrolling
+    const futureConditions = document.querySelector("#future-conditions");
+    futureConditions.innerHTML = "<ul>" + data.map( data =>{
+        return "<li>" + data.dt_txt + "</li>" ;
+    }).join("") + "</ul>"
 
-    //     var datum = new Date(Date.UTC(2021, 1, 9, 6, 4, 3));
-    //     return datum.getTime() / 1000;
+    const futureTemps = document.querySelector("#future-temps");
+    futureTemps.innerHTML = "<ul>" + data.map(data => {
+        const celcius = data.main.feels_like- 273.15;
+        const fahrenheit = 1.8 * (data.main.feels_like- 273) + 32;
+        return "<li>" + `${Math.round(celcius)}°C || ${Math.round(fahrenheit)}°F` + "</li>";
+    }).join("") + "</ul>"
 
-    // function scrollTime(){
-    //     const element = document.getElementById('scrollTime');
-    //     let y = element.scrollTop;
-    //     document.getElementById("future-time").innerHTML = y;
-    //     console.log("hello")
-    // }
-})
+    const futureWeatherCondition = document.querySelector("#future-weathercondition");
+    futureWeatherCondition.innerHTML = "<ul>" + data.map(data => {
+        return "<li>" + data.weather[0].description  + "</li>";
+    }).join("") + "</ul>"
+    
+}
+
+// body.onscroll = logScroll;
+
+// function logScroll() {
+//     data.map(data => {
+//         console.log(data.dt_txt);
+//         console.log(data.main.feels_like);
+//         console.log(data.weather[0].description);
+//     })
+// }
+
+
+
+// document.getElementById("change-morning").onclick = () => {
+//     const time = ""
+//     document.getElementById("time").innerHTML = time
+// }
+
+// document.getElementById("change-afternoon").onclick = function changeMorning() {
+//     const time = ""
+//     document.getElementById("time").innerHTML = time
+// }
+
+// document.getElementById("change-night").onclick = function changeMorning() {
+//     const time = ""
+//     document.getElementById("time").innerHTML = time
+// }
+
+// document.getElementById("change-current").onclick = function changeMorning() {
+//     // TODO: Implement this in order to gain the current time again
+//     document.getElementById("phaseOfDay").innerHTML = currentWeather();
+// }
